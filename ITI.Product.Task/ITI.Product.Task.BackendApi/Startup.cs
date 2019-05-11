9428057b340f521503b0a8164f16b93988240ad2
Ahmadaboelfath <ahmadaboelfath@gmail.com>
2019-05-11 21:32:20 +0200

    Final CORS update

--- ITI.Product.Task/ITI.Product.Task.BackendApi/Startup.cs
+++ ITI.Product.Task/ITI.Product.Task.BackendApi/Startup.cs
@@ -17,6 +17,7 @@ namespace ITI.Product.Task.BackendApi
         }
 
         public IConfiguration Configuration { get; }
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
