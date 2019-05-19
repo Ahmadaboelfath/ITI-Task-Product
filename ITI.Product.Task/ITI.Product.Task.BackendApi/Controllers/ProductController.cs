376e46561fe4dac3cbe30077d35318c9b143c531
Ahmadaboelfath <ahmadaboelfath@gmail.com>
2019-05-19 10:33:28 +0200

    any

--- ITI.Product.Task/ITI.Product.Task.BackendApi/Controllers/ProductController.cs
+++ ITI.Product.Task/ITI.Product.Task.BackendApi/Controllers/ProductController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AutoMapper;
+
 using ITI.Product.Task.BackendApi.Core;
 using ITI.Product.Task.BackendApi.Models;
 using Microsoft.AspNetCore.Http;
